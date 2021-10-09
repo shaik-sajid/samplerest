dc91a74d1df2b6ba08220dbb0017265ab29bd6ed
KHAN <shaik-sajid>
2021-10-09 10:37:55 +0530

    Commit on 09Oct 10:30

--- src/test/java/RestAssured/RestAssured/AppTest.java
+++ src/test/java/RestAssured/RestAssured/AppTest.java
@@ -11,9 +11,29 @@ import io.restassured.response.Response;
  */
 public class AppTest {
 
-	@Test
+	@Test (priority = 2)
 	public void get() {
-		Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+		Response res = RestAssured.get("http://reqres.in/api/users/2");
 		System.out.println("Get status code: " + res.getStatusCode());
+		System.out.println("Body: " + res.getBody().asPrettyString());
+	}
+	
+	@Test (priority = 1)
+	public void post() {
+		Response pres = RestAssured.post("http://reqres.in/api/users/2");
+		System.out.println("Post status code :" + pres.getStatusCode());
+	}
+	
+	@Test (priority = 3)
+	public void put() {
+		Response pures = RestAssured.put("http://reqres.in/api/users/2?name=john");
+		System.out.println("Put status code :" + pures.getStatusCode());
+	}
+	
+	@Test (priority = 4)
+	public void delete() {
+		Response dres = RestAssured.delete("http://reqres.in/api/users/2");
+		System.out.println("Delete status code :" +dres.getStatusCode());
+				
 	}
 }

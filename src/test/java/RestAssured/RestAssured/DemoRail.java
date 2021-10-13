0058763cbf2684e14f74689224d7fb7b812ae798
sajid khan <shaik-sajid>
2021-10-13 23:52:47 +0530

    commit on 13-10-2021 11:52pm

--- src/test/java/RestAssured/RestAssured/DemoRail.java
+++ src/test/java/RestAssured/RestAssured/DemoRail.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -20,15 +21,21 @@ public class DemoRail {
 		
 		
 		
+		
 		given()
-			.get("/index.php?/api/v2/get_case/6")
+			.auth().preemptive().basic("shadabanwar.appz@gmail.com", "Test@123")
+			.get("index.php?/api/v2/get_case/6")
 			.then().statusCode(200);
 }
 	
 	@Test
 	public void get() {
-		Response res = RestAssured.get("https://qaautomationappz.testrail.io/index.php?/api/v2/get_case/4");
+		baseURI = "https://qaautomationappz.testrail.io/";
+		
+		Response res = RestAssured.given().auth().preemptive().basic("shadabanwar.appz@gmail.com", "Test@123")
+				.get("index.php?/api/v2/get_case/6");
 		System.out.println(res.getStatusCode());
+		System.out.println(res.getBody().asPrettyString());
 	}
 	
 	@Test
@@ -37,7 +44,7 @@ public class DemoRail {
 		
 		JSONObject postResponse = new JSONObject();
 		
-		postResponse.put("title", "Updated test case 11");
+		postResponse.put("title", "Updated test case 3:51pm");
 		
 		
 		
@@ -45,16 +52,62 @@ public class DemoRail {
 		
 		baseURI = "https://qaautomationappz.testrail.io/";
 		
-		given()
+		
+		given().auth().preemptive().basic("shadabanwar.appz@gmail.com", "Test@123")
 		.header("Content-Type", "application/json")
 		.accept(ContentType.JSON)
 		.body(postResponse.toJSONString())
 		.when()
-		.post("/index.php?/api/v2/update_case/6")
+		.post("index.php?/api/v2/add_case/1")
 		.then()
 		.statusCode(200)
 		.log().all();
 		
 		System.out.println(postResponse);
+		
 	}
+	
+	@Test
+	public void put() {
+		 Map<String, Object> mp = new HashMap<String, Object>();
+			
+			JSONObject putResponse = new JSONObject();
+			
+			putResponse.put("title", "Updated test case 5:09 pm");
+			
+			
+			
+			System.out.println(putResponse.toJSONString());
+			
+			baseURI = "https://qaautomationappz.testrail.io/";
+			
+			
+			given().auth().preemptive().basic("shadabanwar.appz@gmail.com", "Test@123")
+			.header("Content-Type", "application/json")
+			.accept(ContentType.JSON)
+			.body(putResponse.toJSONString())
+			.when()
+			.post("index.php?/api/v2/update_case/28")
+			.then()
+			.statusCode(200)
+			.log().all();
+			
+			//System.out.println(putResponse);
+	}
+	
+	@Test
+	public void delete() {
+		baseURI = "https://qaautomationappz.testrail.io/";
+		
+		
+		given().auth().preemptive().basic("shadabanwar.appz@gmail.com", "Test@123")
+		.header("Content-Type", "application/json")
+		.accept(ContentType.JSON)
+		.when()
+		.post("index.php?/api/v2/delete_case/28")
+		.then()
+		.statusCode(200)
+		.log().all();
+	}
+	
 }
